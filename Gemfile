03a2954fc0b6c3819b65cb370e6df567543094e9
hkdnet <satoko.itse@gmail.com>
2022-07-16 23:20:56 +0900

    Enable NewRelic
    Completed 200 OK in 33805ms (Views: 11.8ms | ActiveRecord: 37.3ms | Allocations: 949454)

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ gem "puma", "~> 5.0"
 
 gem "graphql"
 
+gem 'newrelic_rpm'
+
 # Use JavaScript with ESM import maps [https://github.com/rails/importmap-rails]
 # gem "importmap-rails"
 
--- app/graphql/graphql_ruby_nr_schema.rb
+++ app/graphql/graphql_ruby_nr_schema.rb
@@ -5,6 +5,9 @@ class GraphqlRubyNrSchema < GraphQL::Schema
   # For batch-loading (see https://graphql-ruby.org/dataloader/overview.html)
   use GraphQL::Dataloader
 
+  # newrelic
+  use(GraphQL::Tracing::NewRelicTracing, set_transaction_name: true)
+
   # GraphQL-Ruby calls this when something goes wrong while running a query:
   def self.type_error(err, context)
     # if err.is_a?(GraphQL::InvalidNullError)
